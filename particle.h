d4b2ee4d068fa615082743b0c49bcc11ff663a78
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2024-04-25 11:06:37 -0300

    Update particle.h

--- particle.h
+++ particle.h
@@ -1,17 +1,20 @@
-#include "shapes.h"
 #ifndef PARTICLE_H
 #define PARTICLE_H
+
+#include "shapes.h"
 
 struct Particle{
     Vector3f pos, vel;
     Color col;
-    float lifetime, size;
+    int lifetime;
+    float size;
 };
-typedef Particle Particle;
+typedef struct Particle Particle;
 
-Particle createParticle(Vector3f pos, Vector3f vel, Vector3f col, float lifetime);
-void updateParticle(Particle p, float g);
+Particle createParticle(Vector3f pos, Vector3f vel, Color col, int lifetime, float size);
+void updateParticle(Particle *p, float g);
 void drawParticle(Particle p);
 void drawParticles(Particle p[], int n_p, float g);
+Particle* generateParticles(int n, int vel_amp, int size_amp, Vector3f pos, Color col1, Color col2, int lifetime, float size);
 
 #endif
